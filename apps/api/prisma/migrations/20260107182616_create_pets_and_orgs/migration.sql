338f1aaac163de32ef605664216cb72887a73c93
Matheus <matheusteodorogarcia@gmail.com>
2026-01-08 00:16:35 +0000

    feat(migration): add pets and orgs tables with enums and constraints

---
+++ apps/api/prisma/migrations/20260107182616_create_pets_and_orgs/migration.sql
@@ -0,0 +1,52 @@
+-- CreateEnum
+CREATE TYPE "Age" AS ENUM ('PUPPY', 'JUNIOR', 'ADULT', 'SENIOR');
+
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('Small', 'Medium', 'Large', 'Huge');
+
+-- CreateEnum
+CREATE TYPE "EnergyLevel" AS ENUM ('ONE', 'TWO', 'THREE', 'FOUR', 'FIVE');
+
+-- CreateEnum
+CREATE TYPE "IndependenceLevel" AS ENUM ('ONE', 'TWO', 'THREE', 'FOUR', 'FIVE');
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "age" "Age" NOT NULL,
+    "size" "Size" NOT NULL,
+    "energy_level" "EnergyLevel" NOT NULL,
+    "independence_level" "IndependenceLevel" NOT NULL,
+    "additional_characteristics" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "org_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "complement" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "orgs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
