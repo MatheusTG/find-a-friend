3d25afa1cd552d149d667fcf59fd958c389c4f3f
Matheus <matheusteodorogarcia@gmail.com>
2026-01-10 16:56:17 +0000

    feat(auth): add authentication use case for orgs
    - add AuthenticateUseCase to validate org credentials
    - compare hashed passwords using bcryptjs
    - fetch org by email via OrgsRepository
    - throw InvalidCredentialsError on invalid email or password

---
+++ apps/api/src/modules/orgs/use-cases/authenticate.use-case.ts
@@ -0,0 +1,37 @@
+import { compare } from "bcryptjs";
+import { Org } from "../entities/org";
+import { OrgsRepository } from "../repositories/orgs.repository";
+import { InvalidCredentialsError } from "@/lib/errors/invalid-credentials.error";
+
+type AuthenticateUseCaseRequest = {
+  email: string;
+  password: string;
+};
+
+type AuthenticateUseCaseResponse = {
+  org: Org;
+};
+
+export class AuthenticateUseCase {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute(request: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const { email, password } = request;
+
+    const org = await this.orgsRepository.findByEmail(email);
+
+    if (!org) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatches = await compare(password, org.password_hash);
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      org,
+    };
+  }
+}
