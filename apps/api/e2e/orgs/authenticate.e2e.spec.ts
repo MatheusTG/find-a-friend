44de129693f858f42a9be458e1cf382d60d3062e
Matheus <matheusteodorogarcia@gmail.com>
2026-01-13 16:13:50 +0000

    test(orgs): add end-to-end test for organization authentication

---
+++ apps/api/e2e/orgs/authenticate.e2e.spec.ts
@@ -0,0 +1,40 @@
+import { createApp } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+
+const app = await createApp();
+
+describe("Authenticate Org E2E Spec", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    await prisma.org.create({
+      data: {
+        number: "123",
+        name: "ONG Amigos dos Pets",
+        email: "contato@amigosdospets.org",
+        password_hash: await hash("123456", 6),
+        phone: "41999999999",
+        cep: "80000-000",
+        neighborhood: "Centro",
+        street: "Rua das Flores, 123",
+        complement: "Sala 2",
+      },
+    });
+
+    const response = await request(app.server).post("/orgs/auth").send({
+      email: "contato@amigosdospets.org",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(200);
+  });
+});
